a04f42f30d6c54b6005add12c720841cd3f3cae6
Jae Choi <j.thechois@gmail.com>
2019-03-01 22:24:58 +0900

    Twilio API connected

--- src/controllers/phoneController.ts
+++ src/controllers/phoneController.ts
@@ -1,5 +1,40 @@
-const searchNumbers = (req, res) => {
-  res.render('index');
+import countries from '../countries';
+import { Request, Response } from 'express';
+import { numbersByCountry, priceByCountry } from '../twilio';
+
+const searchNumbers = async (req: Request, res: Response) => {
+  const {
+    query: { country }
+  } = req;
+
+  let numbers = null;
+  let price;
+  let error = false;
+
+  if (country) {
+    try {
+      const { data: numbersData } = await numbersByCountry(country);
+      numbers = numbersData.available_phone_numbers;
+      const { data: priceData } = await priceByCountry(country);
+      const localPrice = priceData.phone_number_prices.filter(
+        number => number.number_type === 'local'
+      );
+      if (localPrice.length !== 0) {
+        price = localPrice[0].base_price;
+      }
+    } catch (e) {
+      console.log(e);
+      error = true;
+    }
+  }
+
+  res.render('index', {
+    countries,
+    searchingBy: country,
+    numbers,
+    price,
+    error
+  });
 };
 
 export default {
---
+++ src/twilio.ts
@@ -0,0 +1,26 @@
+import axios from 'axios';
+
+const auth = {
+  username: process.env.TWILIO_SID || '',
+  password: process.env.TWILIO_AUTH_TOKEN || ''
+};
+
+const AccountApi = axios.create({
+  baseURL: `https://api.twilio.com/2010-04-01/Accounts/${
+    process.env.TWILIO_SID
+  }`,
+  auth
+});
+
+const PricingApi = axios.create({
+  baseURL: `https://pricing.twilio.com/v1/`,
+  auth
+});
+
+export const numbersByCountry = async (countryCode: string) =>
+  AccountApi.get(
+    `AvailablePhoneNumbers/${countryCode}/Local.json?SmsEnabled=true&ExcludeAllAddressRequired=true`
+  );
+
+export const priceByCountry = async (countryCode: string) =>
+  PricingApi.get(`PhoneNumbers/Countries/${countryCode}`);
