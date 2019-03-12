000177877372478da4424c3d6a1c3d3c72e983b8
Jae Choi <j.thechois@gmail.com>
2019-03-12 23:56:48 +0900

    Email Verification & Log In

--- src/controllers/accountController.ts
+++ src/controllers/accountController.ts
@@ -1,5 +1,7 @@
 import { getPhoneNumbersByName } from '../twilio';
-import { hashPassword } from '../utils';
+import { hashPassword, genSecret, checkPassword } from '../utils';
+import { prisma } from '../../generated/prisma-client';
+import { sendVerificationEmail } from '../mailgun';
 
 const myAccount = async (req, res) => {
   const USERNAME = 'Jae Phone';
@@ -14,18 +16,59 @@ const myAccount = async (req, res) => {
 };
 
 const createAccount = async (req, res) => {
-  if (req.method === 'GET') {
-    res.render('create-account', { title: 'Create An Account' });
-  } else if (req.method === 'POST') {
+  const title = 'Create An Account';
+  let error;
+
+  if (req.method === 'POST') {
     const {
       body: { email, password }
     } = req;
-    const hash = await hashPassword(password);
-    res.render('create-account');
+    try {
+      const exists = await prisma.$exists.user({ email });
+      if (!exists) {
+        const hash = await hashPassword(password);
+        const sercret = genSecret();
+        await prisma.createUser({
+          email,
+          password: hash,
+          verificationSecret: sercret
+        });
+        sendVerificationEmail(email, sercret);
+      } else {
+        error = 'This user has an account already. Maybe try to log in?';
+      }
+    } catch (e) {
+      error = e;
+    }
+    res.render('create-account', { title, error });
   }
+};
+
+const logIn = async (req, res) => {
+  const title = 'Log In';
+  let error;
+
+  if (req.method === 'POST') {
+    const {
+      body: { email, password }
+    } = req;
+    const user = await prisma.user({ email });
+    if (user) {
+      const check = await checkPassword(user.password, password);
+      if (check) {
+      } else {
+        error = 'Wrong Password';
+      }
+    }
+  } else {
+    error = 'This user does not exists. You need to create an account?';
+  }
+
+  res.render('login', { title, error });
 };
 
 export default {
   myAccount,
-  createAccount
+  createAccount,
+  logIn
 };
--- src/mailgun.ts
+++ src/mailgun.ts
@@ -13,10 +13,23 @@ export const sendNewSMSMail = (from: string, body: string, owner: string) => {
     to: 'j.thechois@gmail.com',
     subject: '(1) New Message on Jae Phone',
     html: `
-    <h1>You have a new message</h1>
+    <span>You have a new message</span>
     <strong>From:</strong> ${from}
     <strong>Message:</strong> ${body}
     `
+  };
+  return sendMail(mail);
+};
+
+export const sendVerificationEmail = (to: string, secret: string) => {
+  const mail: mailgun.messages.SendData = {
+    from: 'Jae Phone<j.thechois@gmail.com>',
+    to: 'j.thechois@gmail.com',
+    subject: 'Email Verification',
+    html: `
+      Your email verification secret is <strong>${secret}</strong>
+      Copy paste it on http://localhost:4000/accounts/verify-email
+      `
   };
   return sendMail(mail);
 };
